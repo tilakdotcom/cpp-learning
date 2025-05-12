e4f1d5d6d34d1dc44fbb5fbc07f3c84eda4f3698
tilak-dev <tsingh99991@gmail.com>
2025-05-12 18:17:40 +0530

    Refactor book allocation logic and add main function for execution

--- learning_dsa/BookAllocation.cpp
+++ learning_dsa/BookAllocation.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include<numeric>
 using namespace std;
 
 // Check if allocation is possible with maxPages
@@ -21,4 +22,38 @@ bool isPossible(vector<int> &books, int students, int maxPages) {
     }
 
     return true;
+}
+
+int allocateBooks(vector<int> &books, int students) {
+    if (students > books.size()) return -1;
+
+    int low = *max_element(books.begin(), books.end());
+    int high = accumulate(books.begin(), books.end(), 0);
+    int result = INT_MAX;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (isPossible(books, students, mid)) {
+            result = mid;
+            high = mid - 1; // try for a better (smaller) answer
+        } else {
+            low = mid + 1; // increase pages limit
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> books = {12, 34, 67, 90};
+    int students = 2;
+
+    int result = allocateBooks(books, students);
+    if (result == -1)
+        cout << "Allocation not possible\n";
+    else
+        cout << "Minimum max pages: " << result << endl;
+
+    return 0;
 }
