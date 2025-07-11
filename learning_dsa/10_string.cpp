709fa53f44136d70b41661cec308e4bba735e790
tilak-dev <tsingh99991@gmail.com>
2025-07-11 17:19:26 +0530

    Refactor string operations to implement Kadane's algorithm for maximum subarray sum

--- learning_dsa/10_string.cpp
+++ learning_dsa/10_string.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
-#include <string>
-#include <algorithm> // For reverse
+#include <vector>
+#include <climits>
 using namespace std;
 
+int kadaneMaxSubarraySum(const vector<int>& nums) {
+    int maxSum = INT_MIN;
+    int currentSum = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        currentSum += nums[i];
+        maxSum = max(maxSum, currentSum);
+
+        if (currentSum < 0)
+            currentSum = 0;
+    }
+
+    return maxSum;
+}
+
 int main() {
-    string s = "hello world";
-    cout << "Original: " << s << endl;
-
-    // Length
-    cout << "Length: " << s.length() << endl;
-
-    // Access
-    cout << "First char: " << s[0] << endl;
-
-    // Concatenation
-    string s2 = " C++";
-    s += s2;
-    cout << "After append: " << s << endl;
-
-    // Substring
-    string sub = s.substr(0, 5);
-    cout << "Substring (0-5): " << sub << endl;
-
-    // Find
-    size_t found = s.find("world");
-    if (found != string::npos)
-        cout << "\"world\" found at index: " << found << endl;
-
-    // Erase
-    s.erase(5, 6); // remove " world"
-    cout << "After erase: " << s << endl;
-
-    // Insert
-    s.insert(5, " everyone");
-    cout << "After insert: " << s << endl;
-
-    // Reverse
-    reverse(s.begin(), s.end());
-    cout << "Reversed: " << s << endl;
-
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int maxSum = kadaneMaxSubarraySum(arr);
+    cout << "Maximum Subarray Sum = " << maxSum << endl;
     return 0;
 }
